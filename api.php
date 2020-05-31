80d433e2a9e598477b9802a7ed8cf30e5ab2619c
Benjamin <nimajneb2005@gmx.de>
2020-05-31 16:01:08 +0200

    Auch api.ph überarbeiten

--- api.php
+++ api.php
@@ -6,42 +6,43 @@ include 'login.inc.php';
 //Status ändern
 if (isset($_POST["id"])) {
     //Status vor der Änderung abrufen
-    $statusJetzt = mysqli_fetch_assoc(mysqli_query($db, "SELECT `status` FROM `task` WHERE `id`=".htmlentities($_POST["id"] . " AND `user`='" . $user_name . "'")));
-    $sql1 = "UPDATE `tasks` SET `status`=1 WHERE `id`=" . htmlentities($_POST["id"]) . " AND `user`='" . $user_name . "'";
-    $sql2 = "UPDATE `tasks` SET `status`=0 WHERE `id`=" . htmlentities($_POST["id"]) . " AND `user`='" . $user_name . "'";
-    
-    //SQL senden
-    if (intval($statusJetzt["status"]) === 1) {
-        if (!mysqli_query($db, $sql2)) {
-            http_response_code(500);
-        }
+    $statusJetztSql = $db->prepare("SELECT `status` FROM `tasks` WHERE `id`=? AND `user`=?" );
+    $statusJetztSql -> bind_param('is', intval($_POST["id"]), $user_name);
+    $statusJetztSql -> execute();
+    $statusJetzt = $statusJetztSql -> get_result();
+    $statusJetztSql -> close();
+    $updateSql = $db -> prepare("UPDATE `tasks` SET `status`=? WHERE `id`=? AND `user`=?");
+    $updateSql -> bind_param('iis', $x , $_POST["id"], $user_name);
+    if (intval($statusJetzt -> fetch_assoc()["status"]) === 1) {
+        $x = 0;
     } else {
-        if (!mysqli_query($db, $sql1)) {
-            http_response_code(500);
-        }
+        $x = 1;
     }
+    
+    $updateSql -> execute();
+    $updateSql -> close();
 }
 
 //Löschen
 if (isset($_POST["del"])) {
     //Alle erledigten Aufgaben löschen
     if ($_POST["del"] === "y") {
-        $sql3 = "DELETE FROM `tasks` WHERE `status`=1 AND `user`='" . $user_name . "'";
-        if (!mysqli_query($db, $sql3)) {
-            http_response_code(500);
-        }
+        $del = $db -> prepare("DELETE FROM `tasks` WHERE `status`=1 AND `user`=?");
+        $del -> bind_param('s', $user_name);
+        $del -> execute();
+        $del -> close();
     } 
     //Spezielle Aufgabe löschen (ID)
     elseif (is_numeric($_POST["del"])) {
-        $sql4 = "DELETE FROM `tasks` WHERE `id`=" . $_POST["del"] . " AND `user`='" . $user_name . "'";
-        if (!mysqli_query($db, $sql4)) {
-            http_response_code(500);
-        }
+        $del = $db -> prepare("DELETE FROM `tasks` WHERE `id`=? AND `user`=?");
+        $del -> bind_param('is', intval($_POST["del"]), $user_name);
+        $del -> execute();
+        $del -> close();
     } else {
         http_response_code(404);
     }
 }
 
 //Verbindung schließen
-mysqli_close($db);
+$db->close();
 ?>
